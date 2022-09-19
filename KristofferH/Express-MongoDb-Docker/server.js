2eaa5a70b26396e4e77b55f490ae75fc6b30c70c
Kristoffer Högberg <kristoffer.hogberg@apendo.se>
2022-09-20 00:17:43 +0200

    add simple login-page

--- KristofferH/Express-MongoDb-Docker/server.js
+++ KristofferH/Express-MongoDb-Docker/server.js
@@ -1,28 +1,43 @@
-// Setup the server 
-const express = require('express');
+// Setup the server
+const express = require("express");
+const bodyParser = require("body-parser");
+const router = express.Router();
 const app = express();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const config = require('./db');
+const config = require("./db");
 const PORT = 3005;
 
-// Connect to DB with mongoose
-mongoose
-    .connect(config.DB, function(err, db) {
-        if(err) {
-            console.log('database is not connected')
-        }
-        else {
-            console.log('connected!!')
-        }
-    }
-    );
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-// 
-app.get('/', function(req, res) {
-    res.json( {"title": "Ninjas from Space II"} );
+// add router in the Express app.
+app.use("/", router);
+
+// Connect to DB with mongoose
+mongoose.connect(config.DB, function (err, db) {
+  if (err) {
+    console.log("database is not connected");
+  } else {
+    console.log("connected!!");
+  }
 });
 
-app.listen(PORT, function(){
-    console.log('Your node js server is running on PORT:',PORT);
+router.get("/", function (req, res) {
+  res.sendfile("index.html");
+});
+
+router.post("/login", function (req, res) {
+  var user_name = req.body.user;
+  var password = req.body.password;
+  console.log("User name = " + user_name + ", password is " + password);
+  res.end("yes");
+});
+
+/* app.get("/", function (req, res) {
+  res.json({ title: "Ninjas from Space II" });
+}); */
+
+app.listen(PORT, function () {
+  console.log("Your node js server is running on PORT:", PORT);
 });
