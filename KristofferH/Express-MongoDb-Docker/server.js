219cf2f88d3995aa765bc9748be443d43995eeb3
Kristoffer Högberg <kristoffer.hogberg@apendo.se>
2022-09-25 18:12:18 +0200

    add authorization on all endpoints (except /home)

--- KristofferH/Express-MongoDb-Docker/server.js
+++ KristofferH/Express-MongoDb-Docker/server.js
@@ -3,9 +3,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const mongoose = require("mongoose");
-const path = require('path');
+const path = require("path");
 
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 const config = require("./db");
 const PORT = 3005;
@@ -33,102 +33,125 @@ mongoose.connect(
 const schemaBook = {
   title: String,
   author: String,
-  yearOfRelease: String
+  yearOfRelease: String,
 };
 
 // Create a model
 const Book = mongoose.model("Books", schemaBook);
 
+// Define endpoint for index.html
+app.get("/api/v1/home", (req, res) => {
+  res.sendFile(path.join(__dirname, "/index.html"));
+});
+
 // Define create login-token endpoint
-app.post('/api/v1/login', (req, res) => {
-
+app.post("/api/v1/login", (req, res) => {
   const user = {
-    id:Date.now(),
+    id: Date.now(),
     userEmail: req.body.userEmail,
-    password: req.body.password
-  }
+    password: req.body.password,
+  };
 
-  jwt.sign({user}, 'secretKey', (err, token) => {
+  jwt.sign({ user }, "secretKey", (err, token) => {
     res.json({
-      token
-    })
-  })
-})
+      token,
+    });
+  });
+});
 
 // Define login endpoint
-app.get('/api/v1/profile', verifyToken, (req,res) => {
-
-  jwt.verify(req.token, 'secretKey', (err, authData) => {
-    if(err) {
-      res.sendStatus(403);  
-    }
-    else {
+app.get("/api/v1/profile", verifyToken, (req, res) => {
+  jwt.verify(req.token, "secretKey", (err, authData) => {
+    if (err) {
+      res.sendStatus(403);
+    } else {
       res.json({
         message: "You are logged in to Cyberdyne Systems",
-        userData: authData
-      })
+        userData: authData,
+      });
     }
-  })
-})
+  });
+});
 
-// Implement middle-ware-function for verifying token
+// Implement middleware-function for verifying token
 function verifyToken(req, res, next) {
-  
-  const bearerHeader =  req.headers['authorization'];
-  if (typeof bearerHeader !== 'undefined') {
-
-    const bearer = bearerHeader.split(' ');
+  const bearerHeader = req.headers["authorization"];
+  if (typeof bearerHeader !== "undefined") {
+    const bearer = bearerHeader.split(" ");
     const bearerToken = bearer[1];
     req.token = bearerToken;
     next();
-  }
-  else {
+  } else {
     res.sendStatus(403);
   }
 }
 
-app.get('/api/v1/home', (req, res) => { 
-  res.sendFile(path.join(__dirname, '/index.html'));
-})
-
 // Define CRUD endpoints
-app.get('/api/v1/listallbooks', (req, res) => {
-  Book.find((err, books) => {
-    res.json(books)
-  })
-})
+app.get("/api/v1/listallbooks", verifyToken, (req, res) => {
+  jwt.verify(req.token, "secretKey", (err, authData) => {
+    if (err) {
+      res.sendStatus(403);
+    } else {
+      Book.find((err, books) => {
+        res.json(books);
+      });
+    }
+  });
+});
 
-app.get('/api/v1/listbook/:id', (req, res) => {
-  Book.findById(req.params.id, (err, books) => {
-    res.json(books)
-  })
-})
+app.get("/api/v1/listbook/:id", verifyToken, (req, res) => {
+  jwt.verify(req.token, "secretKey", (err, authData) => {
+    if (err) {
+      res.sendStatus(403);
+    } else {
+      Book.findById(req.params.id, (err, books) => {
+        res.json(books);
+      });
+    }
+  });
+});
 
-app.post("/api/v1/createbook", async (req, res) => {
-  console.log("inside post function...");
+app.post("/api/v1/createbook", verifyToken, async (req, res) => {
+  jwt.verify(req.token, "secretKey", (err, authData) => {
+    if (err) {
+      res.sendStatus(403);
+    } else {
+      const book = new Book({
+        title: req.body.title,
+        author: req.body.author,
+        yearOfRelease: req.body.yearOfRelease,
+      });
 
-  const book = new Book({
-    title: req.body.title,
-    author: req.body.author,
-    yearOfRelease: req.body.yearOfRelease
-  })
+      book.save((err) => {
+        res.json(book);
+      });
+    }
+  });
+});
 
-  book.save((err) => {
-    res.json(book)
-  })
-})
+app.put("/api/v1/updatebook/:id", verifyToken, (req, res) => {
+  jwt.verify(req.token, "secretKey", (err, authData) => {
+    if (err) {
+      res.sendStatus(403);
+    } else {
+      Book.findByIdAndUpdate(req.params.id, req.body, (err) => {
+        res.json({ message: `updating book ${req.params.id}` });
+      });
+    }
+  });
+});
 
-app.put('/api/v1/updatebook/:id', (req, res) => {
-  Book.findByIdAndUpdate(req.params.id, req.body, (err) => {
-    res.json( {message: `updating book ${req.params.id}`} )
-  })
-})
-
-app.delete('/api/v1/deletebook/:id', (req, res) => {
-  Book.findByIdAndDelete(req.params.id, (err) => {
-    res.json( {message: `deleting book ${req.params.id}` })
-  })
-})
+app.delete("/api/v1/deletebook/:id", verifyToken, (req, res) => {
+  jwt.verify(req.token, "secretKey", (err, authData) => {
+    if (err) {
+      res.sendStatus(403);
+    } else {
+      Book.findByIdAndDelete(req.params.id, (err) => {
+        res.json({ message: `deleting book ${req.params.id}` });
+      });
+    }
+  });
+});
 
 // Set up port to listen to
 app.listen(PORT, function () {
